b59bd77db9492a4b36400a18e675974543e4fac5
s <s@jdlab.org>
2019-05-09 16:10:18 +0900

    WIU-918 #comment first expandable row

---
+++ ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit/PropertyGrid/Implementation/ExpandableObjectItemContainerStyleSelector.cs
@@ -0,0 +1,38 @@
+﻿/*************************************************************************************
+
+   Added 2019-05-06
+   by IUEditor
+
+  ***********************************************************************************/
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Xceed.Wpf.Toolkit.PropertyGrid
+{
+  public class ExpandableObjectItemContainerStyleSelector : StyleSelector
+  {
+
+    /// <summary>
+    /// Set outside (xaml)
+    /// </summary>
+    public Style TitleContainerStyle
+    {
+      get;
+      set;
+    }
+
+    public override Style SelectStyle( object item, DependencyObject container )
+    {
+      PropertyItem pItem = item as PropertyItem;
+      if (pItem.Editor == null && pItem.IsExpandable)
+      {
+        return TitleContainerStyle;
+
+      }
+
+      return base.SelectStyle( item, container );
+
+    }
+  }
+}
